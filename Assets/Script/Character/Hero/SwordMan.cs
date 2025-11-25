090095f25c02581b4f1d2be8293cd654e1dc1c30
TBFgunmo <gunmohome123@gmail.com>
2025-11-26 00:57:30 +0700

    swordman slash range fix and flip

--- Assets/Script/Character/Hero/SwordMan.cs
+++ Assets/Script/Character/Hero/SwordMan.cs
@@ -9,6 +9,7 @@ public class SwordMan : Hero, ISlashable
     void Start()
     {
         base.InitializeHero(100, 20, 10, 5, 10);
+
     }
 
     // Update is called once per frame
@@ -39,26 +40,31 @@ public class SwordMan : Hero, ISlashable
                 SlashEffect,
                 SlashPoint.position,
                 SlashPoint.rotation
-            ); 
+            );
+
             BoxCollider2D hitbox = SlashPoint.GetComponent<BoxCollider2D>();
             SpriteRenderer sr = slash.GetComponent<SpriteRenderer>();
-            
+
             if (sr)
             {
-                Vector2 spriteSize = sr.bounds.size;
-                
-                float scaleX = hitbox.size.x / spriteSize.x;
-                float scaleY = hitbox.size.y / spriteSize.y;
+                float worldWidth = sr.sprite.bounds.size.x;
+                float worldHeight = sr.sprite.bounds.size.y;
 
-                slash.transform.localScale = new Vector3(scaleX, scaleY, 1);
+                // ปรับ scale ให้ match hitbox
+                float scaleX = hitbox.size.x / worldWidth;
+                float scaleY = hitbox.size.y / worldHeight;
+
+                float value = SlashPoint.position.x - SlashPoint.parent.position.x;
+                if (value < 0) scaleX *= -1;
+
+                slash.transform.localScale = new Vector3(scaleX, scaleY, 1f);
             }
 
-            // ทำลายเอฟเฟกต์หลัง 0.3 วินาที (แล้วแต่ภาพคุณ)
             Destroy(slash, 0.3f);
         }
-        
-        Vector2 boxSize = new Vector2(AtkRange, AtkRange * 0.6f); 
-        
+
+        //Vector2 boxSize = new Vector2(AtkRange, AtkRange * 0.6f); 
+
         Collider2D[] hits = Physics2D.OverlapBoxAll
         (
             SlashPoint.position,                       // จุดกลาง collider
