3bf9fc5e4eac7ca21ad030e5fd3b349aa762d4bf
Darkvdz <famparintorn@gmail.com>
2025-11-26 07:13:49 +0700

    endlevel1

--- Assets/Script/Character/Hero/Markman.cs
+++ Assets/Script/Character/Hero/Markman.cs
@@ -13,7 +13,8 @@ public class Markman : Hero, IShootable
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        base.InitializeHero(75, 5, 7, 0.5f, 10);
+        //75/5
+        base.InitializeHero(1500, 150, 7, 0.5f, 10);
 
         SkillCD = 10;
         ReloadTime = AtkCD;
